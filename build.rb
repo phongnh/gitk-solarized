0574f24e3d297e196649ec154f5d95b598f2a616
Phong Nguyen <nhphong1406@gmail.com>
2023-09-06 16:55:06 +0700

    Add Solarized Light

--- build.rb
+++ build.rb
@@ -62,6 +62,24 @@ class BuildGitkSolarized
       base3:   "#fdf6e3",
     },
     light: {
+      base2:   "#073642",
+      red:     "#dc322f",
+      green:   "#859900",
+      yellow:  "#b58900",
+      blue:    "#268bd2",
+      magenta: "#d33682",
+      cyan:    "#2aa198",
+      base02:  "#eee8d5",
+      base3:   "#002b36",
+      orange:  "#cb4b16",
+      base1:   "#586e75",
+      base0:   "#657b83",
+      base00:  "#839496",
+      violet:  "#6c71c4",
+      base01:  "#93a1a1",
+      base03:  "#fdf6e3",
+      back:    "#fdf6e3",
+      black:   "#fdf6e3",
     },
   }.freeze
 
@@ -135,5 +153,5 @@ class BuildGitkSolarized
 end
 
 if $0 == __FILE__
-  BuildGitkSolarized.call(ARGV[1])
+  BuildGitkSolarized.call(ARGV[0])
 end
